9f463b459d7670b5d2fe95311cf1831208608308
Iuliia Sokol <avare779@gmail.com>
2022-12-11 13:36:38 +0200

    added initial dependencies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,11 @@ export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
