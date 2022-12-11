ece85fffc50ccdef54caeaa57907c336a311a4de
Iuliia Sokol <avare779@gmail.com>
2022-12-11 20:37:44 +0200

    margin-top removed from sections

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const SectionWrapper = styled.section`
-  margin-top: 24px;
   display: flex;
   flex-direction: column;
   justify-content: center;
